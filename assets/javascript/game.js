134db41c81f2af013be8f0d7aeae20a829ee1c29
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-07-09 13:28:23 -0500

    changed display to visibility so it's always the same size of a container and created a function to set a new value for each crystal every single new game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,9 +20,16 @@ $(document).ready(function () {
     function randomIntFromInterval(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+    // to get new crystals every time user wins or loses
+    function getNewCrystals() {
+        yellow = randomIntFromInterval(1, 12);
+        red = randomIntFromInterval(1, 12);
+        green = randomIntFromInterval(1, 12);
+        blue = randomIntFromInterval(1, 12);
+    }
 
     // update total and check for win
-    function update(crystalValue){
+    function update(crystalValue) {
         total += crystalValue;
         $("#total").text("Your total is: " + total);
 
@@ -33,11 +40,12 @@ $(document).ready(function () {
             randomNumber = randomIntFromInterval(19, 120);
             total = 0;
             $("#random_number").text("Random number is: " + randomNumber);
-            $("#total").text("Your total is: 0"); 
+            $("#total").text("Your total is: 0");
             // to show the button (js and jquery)
             // document.getElementById("my-button").style.display = "block";
-            $("#my-button").css("display", "block");
-    
+            $("#my-button").css("visibility", "visible");
+            getNewCrystals();
+
         }
         if (total === randomNumber) {
             wins++;
@@ -46,18 +54,19 @@ $(document).ready(function () {
             randomNumber = randomIntFromInterval(19, 120);
             total = 0;
             $("#random_number").text("Random number is: " + randomNumber);
-            $("#total").text("Your total is: 0"); 
+            $("#total").text("Your total is: 0");
             // to show the button
-            $("#my-button").css("display", "block");   
+            $("#my-button").css("visibility", "visible");
+            getNewCrystals();
         }
-    }    
+    }
 
     // document.getElementById("randomNumber").innerHTML = randomIntFromInterval(19, 120);
     // click events for each picture
 
     $("#yellow_crystal").on("click", function () {
         update(yellow);
-    
+
     });
     $("#red_crystal").on("click", function () {
         update(red);
@@ -71,17 +80,18 @@ $(document).ready(function () {
         update(blue);
 
     });
-    $("#my-button").click(function(){
+    $("#my-button").click(function () {
         wins = 0;
         losses = 0;
         total = 0
         randomNumber = randomIntFromInterval(19, 120);
         $("#random_number").text("Random number is: " + randomNumber);
-        $("#total").text("Your total is: " + total); 
+        $("#total").text("Your total is: " + total);
         $("#losses").text("Losses: " + losses);
-        $("#wins").text("Wins: " + wins);  
+        $("#wins").text("Wins: " + wins);
         // to hide the button  
-        $("#my-button").css("display", "none");      
-      });
+        $("#my-button").css("visibility", "hidden");
+        getNewCrystals();
+    });
 
 });
